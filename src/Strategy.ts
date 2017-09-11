256e45f471c976a1d038e581618bb4f18d47651b
noberk <421471260@qq.com>
2017-09-12 01:54:27 +0800

    add strategy.ts

---
+++ src/Strategy.ts
@@ -0,0 +1,73 @@
+class Product  {
+     
+    getPrice(): number {
+        return this.price;
+    }
+    getCount(): number {
+        return this.count;
+    }
+    getName(): string {
+        return this.name;
+    }
+    private price: number;
+    private name: string;
+
+    private count: number;
+    constructor(name: string, price: number, count: number) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+     
+    }
+}
+class Bill {
+    private products = Array<Product>();
+    discount : Discount;
+    constructor(discount:Discount) {
+        this.discount=discount;
+    }
+
+    addProduct(product: Product) {
+        this.products.push(product);
+    }
+
+    getProductsPrice(): number {
+        var productsPrice = 0;
+        this.products.forEach((value) => {
+            productsPrice+= this.discount.getPrice(value.getPrice())*value.getCount();
+        })
+        return productsPrice;
+    }
+
+}
+
+abstract class Discount{  
+    abstract getPrice(basePrice:number): number;
+}
+class Normal extends Discount{
+    getPrice(basePrice: number): number {
+        return basePrice;
+    }
+}
+class ReturnPrice extends Discount{
+    getPrice(basePrice: number): number {
+        if(basePrice>=100){
+            return basePrice-20;
+        }else{
+            return basePrice;
+        }
+    }
+}
+class SalePrice extends Discount{
+    getPrice(basePrice: number): number {
+        return basePrice * 0.5;
+    }
+}
+
+var p1 = new Product('yagao', 10, 1);
+var p2 = new Product('rice', 100, 2);
+
+var bill = new Bill(new ReturnPrice);
+bill.addProduct(p1);
+bill.addProduct(p2);
+console.log(bill.getProductsPrice())
